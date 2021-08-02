0c4633ca7efa1250caaee579519a9d9adf4af468
yzleaf <yezulong@163.com>
2021-08-02 10:59:29 -0400

    webgazer comments

--- WebGazer_method/script.js
+++ WebGazer_method/script.js
@@ -6,9 +6,11 @@ var positionY = document.getElementById('positionY')
 
 const LOOK_DELAY = 3000 // 3 second
 
+// set the left and right area as limit area, beyond this area will be reported
 const LEFT_CUTOFF = window.innerWidth / 8
 const RIGHT_CUTOFF = window.innerWidth - window.innerWidth / 8
 
+// set the up and down area as limit area, beyond this area will be reported
 const UP_CUTOFF = window.innerHeight / 8
 const DOWN_CUTOFF = window.innerHeight - window.innerHeight / 8
 
@@ -25,34 +27,36 @@ webgazer
         positionX.innerHTML = "Position: X: " + data.x
         positionY.innerHTML = "Position: Y: " + data.y
 
+        // direction limit area
+        // when we first detect the gaze point out the area, record time and direction
         if (data.x < LEFT_CUTOFF &&
             lookDirection !== "LEFT" &&
             lookDirection !== "RESET" )
         {
             startLookTime = timestamp
             lookDirection = "LEFT"
-            flag = "L"
+            flag = "L" // used to show the direction when alert
         } else if (data.x > RIGHT_CUTOFF &&
                   lookDirection !== "RIGHT" &&
                   lookDirection !== "RESET")
         {
             startLookTime = timestamp
             lookDirection = "RIGHT"
-            flag = "R"
-        }if (data.y < UP_CUTOFF &&
+            flag = "R" // used to show the direction when alert
+        } else if (data.y < UP_CUTOFF &&
                   lookDirection != "UP" &&
                   lookDirection != "RESET")
         {
             startLookTime = timestamp
             lookDirection = "UP"
-            flag = "U"
+            flag = "U" // used to show the direction when alert
         } else if (data.y > DOWN_CUTOFF &&
                   lookDirection != "DOWN" &&
                   lookDirection != "RESET")
         {
             startLookTime = timestamp
             lookDirection = "DOWN"
-            flag = "D"
+            flag = "D" // used to show the direction when alert
         } else if (data.x >= LEFT_CUTOFF &&
                    data.x <= RIGHT_CUTOFF &&
                    data.y >= UP_CUTOFF &&
@@ -62,16 +66,17 @@ webgazer
             lookDirection = null
         }
 
-
+        // time last period
         if (startLookTime + LOOK_DELAY < timestamp) {
             startLookTime = Number.POSITIVE_INFINITY
             lookDirection = "STOP"
             setTimeout(() => {
                 alert("Pay attention to your eyes！ " + flag);
-                lookDirection = "RESET"
+                lookDirection = "RESET" // avoid conflict with the previous operation
             }, 200)
         }
     })
     .begin()
 
+// this command is to hide the video and pupil gaze point
 // webgazer.showVideoPreview(false).showPredictionPoints(false)
